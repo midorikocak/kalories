d551b021b64826a42d0e6c032dec13ca0eb07011
Midori Koçak <mtkocak@gmail.com>
2017-02-03 00:44:23 +0100

    subdirectory

--- App.php
+++ App.php
@@ -9,9 +9,7 @@ new class
     {
         $this->request = new \MidoriKocak\Request();
         $elements = $this->request->urlElements;
-        $kalories = new \MidoriKocak\Kalories(new \PDO("sqlite:kalories/db/database.db"));
-        var_dump($kalories);
-        die;
+        $kalories = new \MidoriKocak\Kalories(new \PDO("sqlite:/kalories/db/database.db"));
         $parameters = $this->request->parameters;
         if (empty($elements)) {
             echo json_encode(['message' => 'welcome to kalories app api.']);
